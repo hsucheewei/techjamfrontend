d04b2bcb01beb25e9f767f0e5994bca9d072098a
Thar Lin Shwe <albent2017@gmail.com>
2024-06-29 21:00:17 +0800

    Implemented dummy page for openAI integration

---
+++ components/chat.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { FaSearch } from "react-icons/fa";
+
+export const Chat = () => {
+    return (
+        <div>
+            <input type="text" /> <button><FaSearch /></button>
+        </div>
+    )
+}
