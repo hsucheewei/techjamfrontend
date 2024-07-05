782b70925a1dbe0bd341ce671dc12ace00a5b39b
Thar Lin Shwe <albent2017@gmail.com>
2024-07-05 15:04:48 +0800

    Implemented shop page and layout

---
+++ app/shop/page.tsx
@@ -0,0 +1,20 @@
+import { Product } from "@/components/product";
+import { Tab } from "@/components/tab";
+import { PRODUCTS } from "@/lib/constant";
+
+export default function ShopPage() {
+    const products = PRODUCTS
+
+    const firstColumn = products.filter((_, index) => index % 2 === 0);
+    const secondColumn = products.filter((_, index) => index % 2 === 1);
+
+    return (
+        <main>
+            <Tab />
+            <div className="flex gap-5 mx-5">
+                <div className="flex flex-col gap-5">{firstColumn.map((product) => <Product key={product.name} {...product} />)}</div>
+                <div className="flex flex-col gap-5">{secondColumn.map((product) => <Product key={product.name} {...product} />)}</div>
+            </div>
+        </main>
+    )
+}
